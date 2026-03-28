f72df27b2ee7dc12f85c086c9ed142f562dedec6
dennismysh <256038340+dennismysh@users.noreply.github.com>
2026-03-28 09:50:39 -0400

    feat: top-level Sigil module re-exports

--- sigil-hs/src/Sigil.hs
+++ sigil-hs/src/Sigil.hs
@@ -1,7 +1,17 @@
 module Sigil
   ( module Sigil.Core.Types
   , module Sigil.Core.Error
+  , module Sigil.Core.Chunk
+  , module Sigil.Codec.Pipeline
+  , module Sigil.IO.Reader
+  , module Sigil.IO.Writer
+  , module Sigil.IO.Convert
   ) where
 
 import Sigil.Core.Types
 import Sigil.Core.Error
+import Sigil.Core.Chunk
+import Sigil.Codec.Pipeline
+import Sigil.IO.Reader
+import Sigil.IO.Writer
+import Sigil.IO.Convert
